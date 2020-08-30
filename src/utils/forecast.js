8ebeff1eb64c35582d6ec0886728904f367e9546
Steve Carlson <stevenralphcarlson@gmail.com>
2020-08-30 16:46:53 -0400

    Deploy to Heroku

---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require("request");
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=4809e04529f598ee773728d178ec71ce&query=" +
+    longitude +
+    "," +
+    latitude +
+    "&units=f";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " degrees out. It feels like " +
+          body.current.feelslike +
+          " degrees out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
